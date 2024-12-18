df8845124ff6417776a6e1109d3cba39c247454e
Achmad Tirto Sudiro <achmadtirtosudirosudiro@gmail.com>
2024-12-18 13:28:50 +0700

    fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('is_admin')->default(0); // 0 = user | 1 = petugas | 2 = admin
+            $table->integer('is_admin')->default(0); // 0 = user | 1 = admin
             $table->string('images')->nullable();
             $table->timestamps();
         });
